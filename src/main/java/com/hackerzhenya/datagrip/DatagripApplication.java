3ff3b59d0551a76526e5ba03863d67b740a2b510
hackerzhenya <zhenya92.ru@gmail.com>
2021-05-31 22:10:26 +0500

    Initial commit

---
+++ src/main/java/com/hackerzhenya/datagrip/DatagripApplication.java
@@ -0,0 +1,11 @@
+package com.hackerzhenya.datagrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagripApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DatagripApplication.class, args);
+	}
+}
